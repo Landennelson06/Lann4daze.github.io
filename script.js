f622b385f83c66365b220c3428df3f24eb028253
Landen Nelson <Landennelson06@Gmail.com>
2020-12-12 20:12:10 -0500

    A bunch of painful stuff :)

--- script.js
+++ script.js
@@ -13,40 +13,43 @@ typewriter
   .typeString('Backend')
   .pauseFor(1000)
   .deleteChars(7)
-  .typeString("Anything Besides Web design")
-  //.start();
-var navbartype = new Typewriter(navbar.children[0], {
+  //.typeString("Anything But Web Design")
+  .start();
+var navbartype = new Typewriter(navbar.children[1], {
   cursor:'_',
-  loop:false
+  loop:false,
+  delay:120,
+  cursorClassName:'cur',
 })
 navbartype
+  .callFunction(function(state){state.elements.cursor.style.display = 'inline';})
   .typeString('Home‎‎‎‏‏‎')
-  .callFunction(function(state) {
-    state.elements.cursor.style.display = 'none';
-    console.log(state.elements);
-    state.elements.container.classList.add('home')
-    state.elements.container.innerHTML = "Home"
-    navbartype2.start()
-  })
+  .callFunction(function(state) {state.elements.cursor.style.display = 'none';navbartype2.start()})
   .start();
 
-var navbartype2 = new Typewriter(navbar.children[1], {
+var navbartype2 = new Typewriter(navbar.children[2], {
   cursor:'_',
-  loop:false
+  loop:false,
+  delay:120,
+  cursorClassName:'cur',
 })
 navbartype2
   //.pauseFor(1000)
+  .callFunction(function(state){state.elements.cursor.style.display = 'inline';})
   .typeString('Portforlio‏‏‎')
-  .callFunction(function(state) {
-    state.elements.cursor.style.display = 'none';
-    navbartype3.start()
-  })
-var navbartype3 = new Typewriter(navbar.children[2], {
+  .callFunction(function(state) {state.elements.cursor.style.display = 'none';navbartype3.start()})
+var navbartype3 = new Typewriter(navbar.children[3], {
   cursor:'_',
-  loop:false
+  loop:false,
+  delay:120,
+  cursorClassName:'cur',
 })
 navbartype3
+  .callFunction(function(state){state.elements.cursor.style.display = 'inline';})
   .typeString('About Me‏‏‎')
   .callFunction(function(state) {state.elements.cursor.style.display = 'none';})
+document.getElementsByClassName('cur')[0].style.display = 'none';
+document.getElementsByClassName('cur')[1].style.display = 'none';
+document.getElementsByClassName('cur')[2].style.display = 'none';
 //fontready closing
 });
